6c4fc832498df88b0ecc46f98408321424d0ffbf
ilyaKupriyanov <kupjob@yandex.ru>
2020-04-18 00:06:56 +0300

    refactoring of task 2

--- src/main/java/com/example/restdocumentreciever/threadSafe/TestClass.java
+++ src/main/java/com/example/restdocumentreciever/threadSafe/TestClass.java
@@ -7,36 +7,31 @@ public class TestClass {
 
     public static void main(String[] args) {
 
-        Function<Integer, Double> function1 = new Function<Integer, Double>() {
+        Function<Integer, Double> IntToDoubleFunc = new Function<Integer, Double>() {
             @Override
             public Double getResult(Integer param) {
                 return param*10.0;
             }
         };
 
-        Function<LocalDate, Month> function2 = new Function<LocalDate, Month>() {
-            @Override
-            public Month getResult(LocalDate param) {
-                return param.getMonth();
-            }
-        };
+        Function<LocalDate, Month> LocalDateToMonthFunc = LocalDate::getMonth;
 
         LocalDate localDateNow = LocalDate.now();
         LocalDate localDate1 = LocalDate.of(2010, 2, 22);
 
         ThreadSafeClass threadSafeClass = new ThreadSafeClass();
 
-        Future<Double> future1 = threadSafeClass.compute(10, function1);
-        Future<Double> future2 = threadSafeClass.compute(12, function1);
+        Future<Double> future1 = threadSafeClass.compute(10, IntToDoubleFunc);
+        Future<Double> future2 = threadSafeClass.compute(12, IntToDoubleFunc);
 
-        Future<Month> future3 = threadSafeClass.compute(localDateNow, function2);
-        Future<Month> future4 = threadSafeClass.compute(localDate1, function2);
+        Future<Month> future3 = threadSafeClass.compute(localDateNow, LocalDateToMonthFunc);
+        Future<Month> future4 = threadSafeClass.compute(localDate1, LocalDateToMonthFunc);
 
-        Future<Double> future5 = threadSafeClass.compute(12, function1);
-        Future<Double> future6 = threadSafeClass.compute(13, function1);
+        Future<Double> future5 = threadSafeClass.compute(12, IntToDoubleFunc);
+        Future<Double> future6 = threadSafeClass.compute(13, IntToDoubleFunc);
 
-        Future<Month> future7 = threadSafeClass.compute(localDateNow, function2);
-        Future<Month> future8 = threadSafeClass.compute(localDate1, function2);
+        Future<Month> future7 = threadSafeClass.compute(localDateNow, LocalDateToMonthFunc);
+        Future<Month> future8 = threadSafeClass.compute(localDate1, LocalDateToMonthFunc);
 
     }
 
